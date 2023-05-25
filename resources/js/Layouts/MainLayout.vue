656a287f5906489f2e8a954a42dbe633bd7f792d
SrZitu <zitu094@gmail.com>
2023-05-26 00:15:13 +0600

    reactive data in vue

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -2,12 +2,15 @@
   <div>Index</div>
   <Link href="/">Main Page</Link>
   <Link href="/show">Show Page</Link>
+  <p>The page timer{{ timer }}</p>
 
   <slot></slot>
 </template>
 
 <script setup>
 import { Link } from "@inertiajs/vue3";
-
+import { ref } from "vue";
+const timer = ref(0);
+setInterval(() => timer.value++, 1000);
 defineProps({ message: String });
 </script>
