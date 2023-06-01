ce05eb60a06f01ba119a76f94656f47e79b6e134
SrZitu <zitu094@gmail.com>
2023-06-02 01:28:43 +0600

    adding flash message

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -34,7 +34,8 @@ class ListingController extends Controller
     public function store(Request $request)
     {
         Listing::create($request->all());
-        return redirect()->route('listing.index')->with('success','Listing is created successfully');
+        return redirect()->route('listing.index')->with('success', 'Listing is created successfully');
+
     }
 
     /**
--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,16 +1,24 @@
 <template>
   <div>Index</div>
-  <Link href="/">Main Page</Link><br>
+  <Link href="/">Main Page</Link><br />
   <Link href="/show">Show Page</Link>
-  <p>The page timer {{ timer }}</p>
+  <div v-if="$page.props.flash.success" class="success">
+    {{ $page.props.flash.success }}
+</div>
+
 
   <slot></slot>
 </template>
 
 <script setup>
+
 import { Link } from "@inertiajs/vue3";
-import { ref } from "vue";
-const timer = ref(0);
-setInterval(() => timer.value++, 1000);
-defineProps({ message: String });
+
 </script>
+
+<style scoped>
+.success {
+  background: green;
+  color: white;
+}
+</style>
